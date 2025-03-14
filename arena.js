3bc3fded3565c5a4783026d0bd6b8738d3585ff5
Aaryan Pashine <pashineaaryan@gmail.com>
2025-03-14 13:52:58 -0400

    updates

--- arena.js
+++ arena.js
@@ -1,26 +1,113 @@
-// -----------------
+// ------------
 // UTILITIES
-// -----------------
-function POST(url, body, headers) { }
+// ------------
 
 /**
 @template T
+@param {string} auth 
+@param {string} url 
+@param {string | Object} body 
 @returns {Promise<T>}
 */
-async function GET(url, headers) {
-  return fetch(url,
-    {
-      headers: { "Access-Control-Allow-Origin": "*" },
-    }
-    // , {headers}
-  ).then((res) => res.json())
+function POST(url, body, auth){
+  let b = typeof body == "object" ? JSON.stringify(body): 
+    typeof body == "string" ? body : console.error("body unexpected")
+  if (!b) throw Error("no body")
+  
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${auth}`,
+      "Content-Type": "application/json",
+      cache: "no-store",
+      "Cache-Control": "max-age=0, no-cache",
+      referrerPolicy: "no-referrer",
+    },
+    body: b
+  }).then((res) => {
+      const contentType = res.headers.get("content-type");
+      if (contentType  && contentType.indexOf("application/json") !== -1) {
+        return res.json()
+      } else {
+        return res.ok
+      }
+  })
 }
-function PUT() { }
-function DELETE() { }
 
-// -----------------
-// MAIN ARENA FUNCTION
-// -----------------
+/**
+@template T
+@param {string} auth 
+@param {string} url 
+@returns {Promise<T>}
+*/
+async function GET(url, auth){
+  console.log("url", url)
+  console.log("auth", auth)
+  return fetch(url, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+      cache: "no-store",
+      "Cache-Control": "max-age=0, no-cache",
+      referrerPolicy: "no-referrer",
+    }
+  }).then((res) => 
+    {
+      const contentType = res.headers.get("content-type");
+      if (contentType  && contentType.indexOf("application/json") !== -1) {
+        return res.json()
+      } else {
+        return res
+      }
+    })
+}
+
+/**
+@template T
+@param {string} auth 
+@param {string} url 
+@param {string | Object} body 
+@returns {Promise<T>}
+*/
+function PUT(url, body, auth){
+  let b = typeof body == "object" ? JSON.stringify(body): 
+    typeof body == "string" ? body : console.error("body unexpected")
+  if (!b) throw Error("no body")
+
+  console.log("body", b)
+  console.log("url", url)
+  
+  return fetch(url, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${auth}`,
+      "Content-Type": "application/json",
+      cache: "no-store",
+      "Cache-Control": "max-age=0, no-cache",
+      referrerPolicy: "no-referrer",
+    },
+    body: b
+  })
+    .then((res) => {
+      const contentType = res.headers.get("content-type");
+      if (contentType  && contentType.indexOf("application/json") !== -1) {
+        return res.json()
+      } else {
+        return res.ok
+      }
+    })
+}
+
+function DELETE(url, auth){
+ return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + auth,
+    },
+    method: "DELETE",
+  })
+}
+
+
 /**
 * @typedef {Object} Options
 * @property {string} auth
@@ -35,19 +122,31 @@ function DELETE() { }
 function Arena(opts) {
   let auth = opts?.auth ? opts.auth : ""
   let host = opts?.host ? opts.host : "https://api.are.na/v2/"
-
+  
   return {
-    channel: {
-      get: (slug) => get_channel(slug, { auth, host }),
-      create_block: () => { }
+    me: async () => {
+      if (!auth) {
+        console.warn("No Auth")  
+        return 
+      }
+
+      return await GET(host + "me", auth)
     },
-    block: {
-      get: (slug) => get_block(slug, { auth, host })
-    }
+    channel: (slug) => ({
+      get: () =>  get_channel(slug, {auth, host}),
+      create_block: (body) => create_block(slug, body, {auth, host}),
+      disconnect_block: (id) => disconnect_block(slug, id, {auth, host}),
+      hack_refresh: () => hack_refresh(slug, {auth, host})
+    }),
+    
+    block: (slug) => ({
+      get: () => get_block(slug, {auth, host}),
+      update: (body) => update_block(slug, body, {auth, host}),
+    })
   }
 }
 
-export { Arena }
+export {Arena}
 
 
 /**
@@ -55,8 +154,18 @@ export { Arena }
 * @param {Options} opts 
 * @returns {Promise<Channel>}
 */
-async function get_channel(slug, opts) {
-  return await GET(opts.host + "channels/" + slug)
+async function get_channel(slug, opts){
+  return await GET(opts.host 
+                   + "channels/" 
+                   + slug 
+                   + "?per=100"
+                   , opts.auth
+                  )
+}
+
+async function hack_refresh(slug, opts){
+  return create_block(slug, {content : "TBR"}, opts)
+    .then((res) => disconnect_block(slug, res.id, opts))
 }
 
 /**
@@ -84,13 +193,71 @@ async function get_channel(slug, opts) {
 */
 
 
+
 /**
 * @param {(string | number)} slug 
 * @param {Options} opts 
 * @returns {Promise<Block>}
 */
-async function get_block(slug, opts) {
-  return await GET(opts.host + "blocks/" + slug, opts)
+async function get_block(slug, opts){
+  return  await
+    GET(opts.host  
+        + "blocks/"  
+        + slug, opts.auth)
+}
+
+
+/**
+* @typedef {Object} CreateBlockRequest
+* @property {string} content
+* @property {string=} source
+* @param {(string | number)} channel_slug 
+* @param {CreateBlockRequest} request_data 
+* @param {Options} opts 
+* @returns {Promise<Block>}
+*/
+async function create_block(channel_slug, request_data, opts){
+  return await POST(opts.host 
+                    + "channels/"
+                    + channel_slug
+                    + "/blocks"
+                    , request_data
+                    , opts.auth
+                   )
+}
+
+/**
+* @typedef {Object} UpdateBlockRequest
+* @property {string=} content
+* @property {string=} title
+* @property {string=} description
+* @param {number} id 
+* @param {UpdateBlockRequest} request_data 
+* @param {Options} opts 
+* @returns {Promise<Block>}
+*/
+async function update_block(id, request_data, opts){
+  return await PUT(opts.host 
+                    + "blocks/"
+                    + id
+                    , request_data
+                    , opts.auth
+                   )
+}
+
+/**
+* @param {number} id 
+* @param {Options} opts 
+*/
+async function disconnect_block(channel_slug, id, opts){
+  console.log("disconnecting from", channel_slug, "block", id )
+  return DELETE(opts.host 
+                + "channels/"
+                + channel_slug
+                + "/blocks/"
+                + id
+                , opts.auth
+               )
 }
 
 /**
@@ -111,4 +278,95 @@ async function get_block(slug, opts) {
   @property {null | { title?: string; url: string; provider: { name: string; url: string; } | null; }} source
   @property {null | { content_type: string; display: { url: string }; filename: string; lage: { url: string }, original: { file_size: number; file_size_display: string; url: string; }; square: { url: string }; thumb: { url: string }; updated_at: Date; }} image
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
